7783789470fa31788d988d94c3f5a60633892998
Unknown <nuno.freitas96@gmail.com>
2018-11-15 23:24:49 +0000

    Continuing Logistics

--- Customer-app/app/src/main/java/org/feup/cmov/customerapp/adapter/OrderAdapter.java
+++ Customer-app/app/src/main/java/org/feup/cmov/customerapp/adapter/OrderAdapter.java
@@ -1,37 +1,66 @@
 package org.feup.cmov.customerapp.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.feup.cmov.customerapp.R;
+import org.feup.cmov.customerapp.model.OrderItem;
+import org.feup.cmov.customerapp.model.Ticket;
+
+import java.util.List;
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.MyViewHolder> {
+
+    List<OrderItem> orderList;
+    LayoutInflater layoutInflater;
+
+    public OrderAdapter(Context context, List<OrderItem> orderList) {
+        this.orderList = orderList;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.order_item, viewGroup, false);
+        OrderAdapter.MyViewHolder holder = new OrderAdapter.MyViewHolder(view);
+
+
+
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
 
+        OrderItem order = orderList.get(i);
+        myViewHolder.setData(order, i);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return orderList.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
 
+        TextView title;
+        TextView number;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.order_name);
+            number = itemView.findViewById(R.id.number_order);
         }
 
-        @Override
-        public String toString() {
-            return super.toString();
+        public void setData(OrderItem order, int i){
+            title.setText(order.getTitle());
+            number.setText(String.valueOf(order.getNumber()));
         }
     }
 }
--- Customer-app/app/src/main/java/org/feup/cmov/customerapp/model/OrderItem.java
+++ Customer-app/app/src/main/java/org/feup/cmov/customerapp/model/OrderItem.java
@@ -2,5 +2,29 @@ package org.feup.cmov.customerapp.model;
 
 public class OrderItem {
 
-    
+    String title;
+    int number;
+
+    public OrderItem(String title, int number) {
+        this.title = title;
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
 }
