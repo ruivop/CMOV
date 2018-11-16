fa791528e2015a2a3909fdd30a2fe714c52d97d0
Unknown <nuno.freitas96@gmail.com>
2018-11-16 02:36:38 +0000

    QR code creation

--- Customer-app/app/src/main/java/org/feup/cmov/customerapp/app/CafeteriaActivity.java
+++ Customer-app/app/src/main/java/org/feup/cmov/customerapp/app/CafeteriaActivity.java
@@ -1,11 +1,15 @@
 package org.feup.cmov.customerapp.app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.feup.cmov.customerapp.R;
@@ -27,10 +31,23 @@ public class CafeteriaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Context context = this;
 
         setContentView(R.layout.activity_cafeteria);
 
         setupRecyclerView();
+
+        Button but_qr = findViewById(R.id.order_button);
+
+
+        but_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, OrderQrActivity.class);
+                intent.putExtra("text",getText());
+                startActivity(intent);
+            }
+        });
 
 
     }
@@ -54,6 +71,17 @@ public class CafeteriaActivity extends AppCompatActivity {
 
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView2.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    public String getText(){
+        String finaltext = "";
+        double totalcost = 0;
+        for(int i = 0; i<orderItemList.size(); i++){
+            finaltext += orderItemList.get(i).getTitle() + ":" + orderItemList.get(i).getNumber() + ";";
+            totalcost += (orderItemList.get(i).getPrice() * orderItemList.get(i).getNumber());
+        }
+        finaltext += "Cost:" + String.valueOf(totalcost);
+        return finaltext;
     }
 
     public void addOrder(String title, String price){
---
+++ Customer-app/app/src/main/java/org/feup/cmov/customerapp/app/OrderQrActivity.java
@@ -0,0 +1,51 @@
+package org.feup.cmov.customerapp.app;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import org.feup.cmov.customerapp.R;
+
+public class OrderQrActivity extends AppCompatActivity {
+
+    private ImageView qrCodeImageview;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_qr);
+
+        ImageView iV = findViewById(R.id.qr_image);
+
+        String text = getIntent().getStringExtra("text");
+        try {
+
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+
+            int height = bitMatrix.getHeight();
+            int width = bitMatrix.getWidth();
+
+            final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++){
+                for (int y = 0; y < height; y++){
+                    bmp.setPixel(x, y, bitMatrix.get(x,y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+
+            iV.setImageBitmap(bmp);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
