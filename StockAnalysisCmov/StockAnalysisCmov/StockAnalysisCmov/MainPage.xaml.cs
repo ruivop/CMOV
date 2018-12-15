366fdd989c168f9509923829a18c0dbd3df38b2c
Unknown <nuno.freitas96@gmail.com>
2018-12-15 16:14:11 +0000

    Graph Fully Finished Proceeding to Picker

--- StockAnalysisCmov/StockAnalysisCmov/StockAnalysisCmov/MainPage.xaml.cs
+++ StockAnalysisCmov/StockAnalysisCmov/StockAnalysisCmov/MainPage.xaml.cs
@@ -30,7 +30,7 @@ namespace StockAnalysisCmov
             System.Diagnostics.Debug.WriteLine(MainQuotes);
             
 
-            Title = "Simple Circle";
+            Title = "Graph";
             
             
             //SKCanvasView canvasView = new SKCanvasView();
@@ -64,6 +64,38 @@ namespace StockAnalysisCmov
                 Color = Color.Red.ToSKColor(),
                 StrokeWidth = 0.7f
             };
+            float[] fl = new float[] {4,4};
+            float[] fl2 = new float[] {6,6};
+            SKPaint paintDash = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Gray.ToSKColor(),
+                StrokeWidth = 2f,
+                PathEffect = SKPathEffect.CreateDash(fl, 20)
+            };
+            SKPaint paintDot = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Gray.ToSKColor(),
+                StrokeWidth = 0.7f,
+                PathEffect = SKPathEffect.CreateDash(fl2, 20)
+            };
+
+            SKPaint npaintDash = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Gray.ToSKColor(),
+                StrokeWidth = 0.7f,
+                PathEffect = SKPathEffect.CreateDash(fl, 20)
+            };
+            SKPaint textPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Black,
+                StrokeWidth = 2f
+            };
+
+
             //canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
             float max = 0;
             float min = 0;
@@ -100,62 +132,116 @@ namespace StockAnalysisCmov
 
             if (MainQuotes.Count > SecondaryQuotes.Count)
             {
-                canvas.Scale(1, 5f, info.Width / MainQuotes.Count, min);
+                canvas.Scale(1, 3f, info.Width / MainQuotes.Count, min);
             }
             else
             {
-                canvas.Scale(1, 5f, info.Width / SecondaryQuotes.Count, min);
+                canvas.Scale(1, 3f, info.Width / SecondaryQuotes.Count, min);
             }
-
+            
+            
             if (MainQuotes.Count != 0)
             {
 
                 SKPath path = new SKPath();
+                SKPath npath = new SKPath();
 
-                canvas.DrawLine(0, canvas.LocalClipBounds.Bottom, info.Width, canvas.LocalClipBounds.Bottom, box);
-                canvas.DrawLine(0, 0, 0, canvas.LocalClipBounds.Bottom, box);
+                double mmax = MainQuotes.OrderByDescending(l => l.Close).ToList()[0].Close;
+                double mmin = MainQuotes.OrderByDescending(l => l.Close).ToList()[MainQuotes.Count - 1].Close;
+
+
+
+                canvas.DrawLine(0, canvas.LocalClipBounds.Bottom - 35, info.Width, canvas.LocalClipBounds.Bottom - 35, box);
+                canvas.DrawLine(0, 0, 0, canvas.LocalClipBounds.Bottom - 35, box);
                 
-                path.MoveTo(0, canvas.LocalClipBounds.Bottom);
-                path.LineTo(0, (float)MainQuotes[0].Close);
+                path.MoveTo(0, canvas.LocalClipBounds.Bottom - 35);
+                path.LineTo(0, canvas.LocalClipBounds.Bottom - (float)MainQuotes[0].Close - 35);
+                
+
                 int last = 0;
                 for (var i = 0; i < MainQuotes.Count - 1; i++)
                 {
-                    path.LineTo(info.Width / MainQuotes.Count * (i+1), (float)MainQuotes[i+1].Close);
-                    canvas.DrawLine(info.Width / MainQuotes.Count * i, (float)MainQuotes[i].Close, info.Width / MainQuotes.Count * (i + 1), (float)MainQuotes[i + 1].Close, paint);
+                    path.LineTo(info.Width / MainQuotes.Count * (i+1), canvas.LocalClipBounds.Bottom -(float)MainQuotes[i+1].Close - 35);
+                    canvas.DrawLine(info.Width / MainQuotes.Count * i, canvas.LocalClipBounds.Bottom - (float)MainQuotes[i].Close - 35, info.Width / MainQuotes.Count * (i + 1), canvas.LocalClipBounds.Bottom - (float)MainQuotes[i + 1].Close - 35, paint);
                     last = i+1;
+                    double current = MainQuotes[i+1].Close;
+
+                    if (current.Equals(mmax) || current.Equals(mmin))
+                    {
+                        canvas.DrawText(Convert.ToString(MainQuotes[i+1].Close), info.Width / MainQuotes.Count * (i+1), canvas.LocalClipBounds.Bottom - (float)MainQuotes[i + 1].Close - 35, textPaint);
+                    }
+
+                        if (i % 3 == 0)
+                    {
+                        npath.MoveTo(info.Width / MainQuotes.Count * (i + 1), canvas.LocalClipBounds.Bottom - (float)MainQuotes[i + 1].Close - 35);
+                        npath.LineTo(info.Width / MainQuotes.Count * (i+1), canvas.LocalClipBounds.Bottom - 35);
+                        canvas.Save();
+                        canvas.Translate((info.Width / MainQuotes.Count * (i+1))- 5, canvas.LocalClipBounds.Bottom - 25);
+                        canvas.RotateDegrees(22);
+                        canvas.DrawText(MainQuotes[i+1].Date, 0, 0, textPaint);
+                        canvas.Restore();
+
+                    }
                 }
-                path.LineTo(info.Width / MainQuotes.Count * last, canvas.LocalClipBounds.Bottom);
+                double first = MainQuotes[0].Close;
+                if (first.Equals(mmax) || first.Equals(mmin))
+                {
+                    canvas.DrawText(Convert.ToString(MainQuotes[0].Close), info.Width / MainQuotes.Count * (0), canvas.LocalClipBounds.Bottom - (float)MainQuotes[0].Close - 35, textPaint);
+                }
+                path.LineTo(info.Width / MainQuotes.Count * last, canvas.LocalClipBounds.Bottom - 35);
                 path.Close();
                 paint.Color = SKColors.Red.WithAlpha((byte)(0xFF * 0.2));
                 canvas.DrawPath(path, paint);
+                canvas.DrawPath(npath, paintDash);
+                canvas.DrawLine(0, canvas.LocalClipBounds.Bottom - (float)MainQuotes.OrderByDescending(l => l.Close).ToList()[0].Close - 35, canvas.LocalClipBounds.Right, canvas.LocalClipBounds.Bottom - (float)MainQuotes.OrderByDescending(l => l.Close).ToList()[0].Close - 35, paintDot);
+                canvas.DrawLine(0, canvas.LocalClipBounds.Bottom - (float)MainQuotes.OrderByDescending(l => l.Close).ToList()[MainQuotes.Count - 1].Close - 35, canvas.LocalClipBounds.Right, canvas.LocalClipBounds.Bottom - (float)MainQuotes.OrderByDescending(l => l.Close).ToList()[MainQuotes.Count - 1].Close - 35, paintDot);
+                }
 
-
-            }
-
-            if(SecondaryQuotes.Count != 0)
+            if (SecondaryQuotes.Count != 0)
             {
-
-                
-                
                 SKPath path = new SKPath();
-                path.MoveTo(0, canvas.LocalClipBounds.Bottom);
-                path.LineTo(0, (float)SecondaryQuotes[0].Close);
+
+                double smax = SecondaryQuotes.OrderByDescending(l => l.Close).ToList()[0].Close;
+                double smin = SecondaryQuotes.OrderByDescending(l => l.Close).ToList()[SecondaryQuotes.Count - 1].Close;
+
+                path.MoveTo(0, canvas.LocalClipBounds.Bottom - 35);
+                path.LineTo(0, canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes[0].Close - 35);
                 int last = 0;
+                
                 paint.Color = Color.Blue.ToSKColor();
                 for (var i = 0; i < SecondaryQuotes.Count - 1; i++)
                 {
-                    path.LineTo(info.Width / SecondaryQuotes.Count * (i + 1), (float)SecondaryQuotes[i + 1].Close);
-                    canvas.DrawLine(info.Width / SecondaryQuotes.Count * i, (float)SecondaryQuotes[i].Close, info.Width / SecondaryQuotes.Count * (i + 1), (float)SecondaryQuotes[i + 1].Close, paint);
+                    path.LineTo(info.Width / SecondaryQuotes.Count * (i + 1), canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes[i + 1].Close - 35);
+                    canvas.DrawLine(info.Width / SecondaryQuotes.Count * i, canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes[i].Close - 35, info.Width / SecondaryQuotes.Count * (i + 1), canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes[i + 1].Close - 35, paint);
                     last = i + 1;
+
+                    double current = SecondaryQuotes[i + 1].Close;
+
+                    if (current.Equals(smax) || current.Equals(smin))
+                    {
+                        canvas.DrawText(Convert.ToString(SecondaryQuotes[i + 1].Close), info.Width / SecondaryQuotes.Count * (i + 1), canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes[i + 1].Close - 35, textPaint);
+                    }
                 }
-                path.LineTo(info.Width / SecondaryQuotes.Count * last, canvas.LocalClipBounds.Bottom);
+
+                double first = SecondaryQuotes[0].Close;
+                if (first.Equals(smax) || first.Equals(smin))
+                {
+                    canvas.DrawText(Convert.ToString(SecondaryQuotes[0].Close), info.Width / SecondaryQuotes.Count * (0), canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes[0].Close - 35, textPaint);
+                }
+                path.LineTo(info.Width / SecondaryQuotes.Count * last, canvas.LocalClipBounds.Bottom - 35);
                 path.Close();
                 paint.Color = SKColors.Blue.WithAlpha((byte)(0xFF * 0.2));
                 canvas.DrawPath(path, paint);
+                paintDot.Color = Color.Black.ToSKColor();
+                canvas.DrawLine(0, canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes.OrderByDescending(l => l.Close).ToList()[0].Close - 35, canvas.LocalClipBounds.Right, canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes.OrderByDescending(l => l.Close).ToList()[0].Close - 35, paintDot);
+                canvas.DrawLine(0, canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes.OrderByDescending(l => l.Close).ToList()[SecondaryQuotes.Count - 1].Close - 35, canvas.LocalClipBounds.Right, canvas.LocalClipBounds.Bottom - (float)SecondaryQuotes.OrderByDescending(l => l.Close).ToList()[SecondaryQuotes.Count - 1].Close - 35, paintDot);
+
             }
 
-            
+
         }
+
+        
     }
 
 
